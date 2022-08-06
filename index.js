67eb74493877022623dfb39407502002558ffa08
Sorin Sarbu <simisarbu@yahoo.com>
2022-08-06 17:18:26 +0300

    simplify map

--- index.js
+++ index.js
@@ -12,16 +12,7 @@ function getTeamHTML(team) {
 }
 
 function displayTeams(teams) {
-  // transforma in html
-  // var teamsHTML = "";
-  // teams.forEach(function (team) {
-  // teamsHTML += getTeamHTML(team);
-  // });
-
-  var teamsHTML = teams.map(function (team) {
-    return getTeamHTML(team);
-  });
-  console.warn("teamsHTML", teamsHTML);
+  var teamsHTML = teams.map(getTeamHTML);
 
   // afisare
   document.querySelector("table tbody").innerHTML = teamsHTML.join("");
