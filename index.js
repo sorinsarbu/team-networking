ec8f1773b2c6558f70a5b0404500278b6ccfd381
Sorin Sarbu <simisarbu@yahoo.com>
2022-08-06 15:53:03 +0300

    first teams list version

---
+++ index.js
@@ -0,0 +1,37 @@
+function displayTeams(teams) {
+  console.warn("display", teams);
+  // var teamsHTML = "";
+  // for (var i = 0; i < teams.length; i++) {
+  // teamsHTML += "<tr><td>TODO</td></tr>";
+  //  }
+  // console.warn(teamsHTML);
+
+  // transforma in html
+  var teamsHTML = "";
+  teams.forEach(function (team) {
+    teamsHTML += `
+        <tr>
+        <td>${team.promotion}</td>
+        <td>${team.members}</td>
+        <td>${team.name}</td>
+        <td>
+        <a href="${team.url}">open</a>
+        </td>
+        <td>x e</td>
+      </tr>`;
+  });
+  // afisare
+  document.querySelector("table tbody").innerHTML = teamsHTML;
+}
+
+function loadTeams() {
+  fetch("data/teams.json")
+    .then(function (r) {
+      return r.json();
+    })
+    .then(function (teams) {
+      displayTeams(teams);
+    });
+}
+
+loadTeams();
