e156c8993df4b77c7db814a16042a6235ddc65f4
Sorin Sarbu <simisarbu@yahoo.com>
2022-08-08 21:01:53 +0300

    use const instead of var

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function getTeamHTML(team) {
 }
 
 function displayTeams(teams) {
-  var teamsHTML = teams.map(getTeamHTML);
+  const teamsHTML = teams.map(getTeamHTML);
 
   // afisare
   $("table tbody").innerHTML = teamsHTML.join("");
@@ -34,12 +34,12 @@ function $(selector) {
 
 function submitForm(e) {
   e.preventDefault();
-  var promotion = $("input[name=promotion]").value;
-  var members = $("input[name=members]").value;
-  var name = $("input[name=name]").value;
-  var url = $("input[name=url]").value;
+  const promotion = $("input[name=promotion]").value;
+  const members = $("input[name=members]").value;
+  const name = $("input[name=name]").value;
+  const url = $("input[name=url]").value;
 
-  var team = {
+  const team = {
     promotion: promotion,
     members: members,
     name: name,
